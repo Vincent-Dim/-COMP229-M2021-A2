03ca41b11172dcb6894552dc3e25c542e3da6457
Vincent Dim <vdim@my.centennialcollege.ca>
2021-06-18 17:36:54 -0400

    added contact list link to NAV

---
+++ .history/Server/Models/contact_20210618102858.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+const Schema = mongoose_1.default.Schema;
+const contactSchema = new Schema({
+    username: String,
+    password: String,
+    email: String,
+    city: String,
+    profession: String,
+    age: Number
+}, {
+    collection: "contact"
+});
+const Model = mongoose_1.default.model("contact", contactSchema);
+exports.default = Model;
+//# sourceMappingURL=contact.js.map
